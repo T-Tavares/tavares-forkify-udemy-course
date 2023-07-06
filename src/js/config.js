8c7fdfe38255a7d5adb9f47883814936d02a2aa1
T-Tavares <thiago.tavares.freire@gmail.com>
2023-07-06 23:57:22 +1200

    Dev Menu v1

--- src/js/config.js
+++ src/js/config.js
@@ -4,5 +4,5 @@ export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const MODAL_CLOSE_SEC = 1.5;
-export const API_KEY = process.env.API_KEY;
-export const DEV_PASS = process.env.DEV_PASS;
+export const API_KEY = process.env.API_KEY || NETLIFY_API_KEY;
+export const DEV_PASS = process.env.DEV_PASS || NETLIFY_DEV_PASS;
